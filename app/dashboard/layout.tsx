0d7285650bf9c026a87e2ba5fd108c36c5cc11b3
Aditya Kumar Puri <113080776+puri-adityakumar@users.noreply.github.com>
2025-06-30 02:06:11 +0530

    Updated layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default async function DashboardLayout({
   } = await supabase.auth.getUser()
 
   if (!user) {
-    redirect('/auth/signin?redirectTo=/dashboard')
+    redirect('/auth/signin')
   }
 
   return (
